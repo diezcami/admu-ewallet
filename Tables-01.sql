2d5cd696db5a7e06e263d0ddfd6015128a60af2a
Cami Diez <diezcami@gmail.com>
2015-10-12 13:12:04 +0800

    Updated tables

--- Tables-01.sql
+++ Tables-01.sql
@@ -1,18 +1,22 @@
 CREATE TABLE user (
     ID_Number           INTEGER(6) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    balance             DOUBLE(6) NOT NULL
-    pin                 INTEGER(6) NOT NULL
+    balance             FLOAT(6) NOT NULL
+    pin                 INTEGER(4) NOT NULL
     name                VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE buy_transaction (
-    Buy_Transaction_ID
-    timestamp
-
+    Buy_Transaction_ID INTEGER(6) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    TODO: timestamp
+    TODO: total cost
+    TODO: articles
 );
 
 CREATE TABLE shop_terminal (
-
+    Shop_Terminal_ID    INTEGER(3) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    balance             FLOAT(8) NOT NULL
+    pin                 INTEGER(4) NOT NULL
+    TODO: SALES
 );
 
 CREATE TABLE order (
@@ -26,17 +30,19 @@ CREATE TABLE order (
 CREATE TABLE item (
     Item_ID             INTEGER(3) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name                VARCHAR(20) NOT NULL
-    cost                DOUBLE(6) NOT NULL
+    cost                FLOAT(6) NOT NULL
 );
 
 CREATE TABLE load_transaction (
     Load_Transaction_ID INTEGER(6) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    amount_loaded       DOUBLE(6) NOT NULL
+    amount_loaded       FLOAT(6) NOT NULL
     ID_Number           INTEGER(6) ZEROFILL NOT NULL
     FOREIGN KEY (ID_Number) REFERENCES user(ID_Number) ON DELETE CASCADE ON UPDATE RESTRICT
 
 );
 
 CREATE TABLE load_terminal (
-
+    Load_Terminal_ID    INTEGER(3) ZEROFILL NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    pin                 INTEGER(4) NOT NULL
+    TODO: LOADS    
 );
