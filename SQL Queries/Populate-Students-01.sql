cee5e161222865390906e85b6ece7cdccab79962
Cami Diez <diezcami@gmail.com>
2015-10-15 00:09:01 +0800

    Restructured files, added queries

---
+++ SQL Queries/Populate-Students-01.sql
@@ -0,0 +1,25 @@
+INSERT INTO user(ID_number, balance, pin, first_name, last_name) 
+VALUES 
+(144107, 0, 0000, 'Willard', 'Torres'),
+(123860, 0, 0000, 'Adrian', 'Adrian'),
+(104233, 0, 0000, 'Chris', 'Amanse'),
+(120497, 0, 0000, 'Kristine', 'Belmonte'),
+(141631, 0, 0000, 'Jasper Lorenz', 'Francisco'),
+
+(152217, 0, 0000, 'Mikee', 'Jazmines'),
+(133998, 0, 0000, 'Andre', 'Tan'),
+(133821, 0, 0000, 'Enrico', 'Sison'),
+(122733, 0, 0000, 'Jace', 'Monje'),
+(140049, 0, 0000, 'Bea', 'Adajar'),
+
+(130440, 0, 0000, 'Breginald Jino', 'Basilio'),
+(140414, 0, 0000, 'Gabrielle Ann', 'Barbudo'),
+(133650, 0, 0000, 'Jenina Isabel', 'Sanchez'),
+(150743, 0, 0000, 'Tomy', 'Callanta'),
+(131225, 0, 0000, 'Ian', 'De La Cruz'),
+
+(140270, 0, 0000, 'Carlos', 'Arcenas'),
+(121451, 0, 0000, 'Pia Melissa', 'Encarnacion'),
+(142732, 0, 0000, 'Railey', 'Montalan'),
+(132408, 0, 0000, 'Aemielvin', 'Loremia'),
+(131356, 0, 0000, 'Camille', 'Diez');
